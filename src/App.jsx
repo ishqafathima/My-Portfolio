bfd889c6de82aeeeb3cbaaead10149d315ae4db2
ishqafathima <149235722+ishqafathima@users.noreply.github.com>
2026-09-07 14:12:54 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Home from "./sections/Home/Home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
