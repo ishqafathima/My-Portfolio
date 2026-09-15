b3d8b02450b2efcd3ccf17eb0cc63545a5a171a5
Ishqa Lafir <ishqafathima34@gmail.com>
2026-09-16 00:14:48 +0530

    update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 
 import Home from "./sections/Home/Home";
 import About from "./sections/About/About";
-import Skills from  "./sections/Skills/Skills";
-import Projects from  "./sections/Projects/Projects";
-import Contact from "./sections/Contact/Contact";
+import Skills from "./sections/Skills/Skills";
+import Projects from "./sections/Projects/Projects";
 import Experience from "./sections/Experience/Experience";
-
-
+import Contact from "./sections/Contact/Contact";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/experience" element={<Experience />} />
-      </Routes>
-    </BrowserRouter>
+      <section id="home">
+        <Home />
+      </section>
+
+      <section id="about">
+        <About />
+      </section>
+
+      <section id="skills">
+        <Skills />
+      </section>
+
+      <section id="projects">
+        <Projects />
+      </section>
+
+      <section id="experience">
+        <Experience />
+      </section>
+
+      <section id="contact">
+        <Contact />
+      </section>
+    </>
   );
 }
 
