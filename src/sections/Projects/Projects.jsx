745b373ed54f218a663b57f8a92b1fa7f698ec27
Ishqa Lafir <ishqafathima34@gmail.com>
2026-09-15 23:46:42 +0530

    update portfolio

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -73,15 +73,15 @@ function Projects() {
         </div>
 
         {/* View All Projects */}
-        <div className="view-all-projects">
-          <a
-            href="https://github.com/ishqafathima?tab=repositories"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            View All Projects →
-          </a>
-        </div>
+       <div className="view-all-projects">
+  <a
+    href="https://github.com/ishqafathima?tab=repositories"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    View All Projects →
+  </a>
+</div>
 
       </div>
     </section>
