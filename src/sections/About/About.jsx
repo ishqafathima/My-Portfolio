826e6b8ad5a6aa7f4750674410c2ff8d5b7de4a8
Ishqa Lafir <ishqafathima34@gmail.com>
2026-09-21 22:48:07 +0530

    update portfolio

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -4,8 +4,8 @@ import { FaGithub, FaLinkedin, FaMapMarkerAlt, FaEnvelope, FaPhone, FaBriefcase 
 function About() {
 const downloadCV = () => {
 const link = document.createElement("a");
-link.href = "/ishqa.pdf";
-link.download = "ishqa.pdf";
+link.href = "/Ishqa.pdf";
+link.download = "Ishqa.pdf";
 link.click();
 };
 
